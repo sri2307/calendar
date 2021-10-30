bb6f4cc27497dd35bf04a1499a3a9d4077b86e94
Srikanth Marrapu <srikanth23111995@gmail.com>
2021-10-30 18:47:34 +0530

    google calender rendered

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -2,17 +2,20 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import GoogleCalendar from "./GoogleCalendar";
 
 const events = [
   {
     title: "Live Class",
     start: new Date(2021, 9, 7),
     end: new Date(2021, 9, 7),
+    category: "live",
   },
   {
     title: "Demo Class",
     start: new Date(2021, 9, 8),
     end: new Date(2021, 9, 8),
+    category: "announcements",
   },
 ];
 
@@ -25,8 +28,13 @@ const MyCalendar = (props) => (
       events={events}
       startAccessor='start'
       endAccessor='end'
+      eventPropGetter={(event) => {
+        const backgroundColor = event.category === "live" ? "red" : "blue";
+        return { style: { backgroundColor } };
+      }}
       style={{ height: 500 }}
     />
+    <GoogleCalendar />
   </div>
 );
 
